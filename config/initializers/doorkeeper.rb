4fd47ebbacf593e423c687fe8421270dcace295a
Dusan Orlovic <duleorlovic@gmail.com>
2020-12-13 10:21:52 +0100

    Enable password flow

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -5,12 +5,16 @@ Doorkeeper.configure do
   # Check the list of supported ORMs here: https://github.com/doorkeeper-gem/doorkeeper#orms
   orm :active_record
 
-  # This block will be called to check whether the resource owner is authenticated or not.
-  resource_owner_authenticator do
-    raise "Please configure doorkeeper resource_owner_authenticator block located in #{__FILE__}"
-    # Put your resource owner authentication logic here.
-    # Example implementation:
-    #   User.find_by(id: session[:user_id]) || redirect_to(new_user_session_url)
+  # # This block will be called to check whether the resource owner is authenticated or not.
+  # resource_owner_authenticator do
+  #   raise "Please configure doorkeeper resource_owner_authenticator block located in #{__FILE__}"
+  #   # Put your resource owner authentication logic here.
+  #   # Example implementation:
+  #   #   User.find_by(id: session[:user_id]) || redirect_to(new_user_session_url)
+  # end
+
+  resource_owner_from_credentials do |_routes|
+    User.authenticate(params[:email], params[:password])
   end
 
   # If you didn't skip applications controller from Doorkeeper routes in your application routes.rb
@@ -28,6 +32,9 @@ Doorkeeper.configure do
   #     redirect_to sign_in_url
   #   end
   # end
+  # admin_authenticator do
+  admin_authenticator do
+  end
 
   # You can use your own model classes if you need to extend (or even override) default
   # Doorkeeper models such as `Application`, `AccessToken` and `AccessGrant.
@@ -296,6 +303,7 @@ Doorkeeper.configure do
   # allow_blank_redirect_uri do |grant_flows, client|
   #   client.superapp?
   # end
+  allow_blank_redirect_uri true
 
   # Specify how authorization errors should be handled.
   # By default, doorkeeper renders json errors when access token
@@ -345,6 +353,7 @@ Doorkeeper.configure do
   #   http://tools.ietf.org/html/rfc6819#section-4.4.3
   #
   # grant_flows %w[authorization_code client_credentials]
+  grant_flows %w[password]
 
   # Allows to customize OAuth grant flows that +each+ application support.
   # You can configure a custom block (or use a class respond to `#call`) that must
@@ -428,6 +437,9 @@ Doorkeeper.configure do
   # skip_authorization do |resource_owner, client|
   #   client.superapp? or resource_owner.admin?
   # end
+  skip_authorization do
+    true
+  end
 
   # Configure custom constraints for the Token Introspection request.
   # By default this configuration option allows to introspect a token by another
